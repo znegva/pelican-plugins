259e0d8e785cdd90a2c016575fdad3f55cb3bbb1
Martin Kausche <github@martinkausche.de>
2015-09-16 17:03:22 +0200

    added ability to define some extra infos for images (titel, alt) added sorting by filename

--- gallery/gallery.py
+++ gallery/gallery.py
@@ -1,26 +1,52 @@
 import os
 from pelican import signals
+from ConfigParser import SafeConfigParser
+import codecs
 
+IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.gif', '.bmp']
+
+class galleryimage:
+    def __init__(self, src, alt, title):
+        self.src = src
+        self.alt = alt
+        self.title = title
+    def __repr__(self):
+        return repr((self.src, self.alt, self.title))
 
 def add_gallery_post(generator):
 
     contentpath = generator.settings.get('PATH')
     gallerycontentpath = os.path.join(contentpath,'images/gallery')
+    parser = SafeConfigParser();
 
     for article in generator.articles:
         if 'gallery' in article.metadata.keys():
             album = article.metadata.get('gallery')
             galleryimages = []
 
-            articlegallerypath=os.path.join(gallerycontentpath, album)
+            albumpath=os.path.join(gallerycontentpath, album)
 
-            if(os.path.isdir(articlegallerypath)):
-                for i in os.listdir(articlegallerypath):
-                    if os.path.isfile(os.path.join(os.path.join(gallerycontentpath, album), i)):
-                        galleryimages.append(i)
+            if(os.path.isdir(albumpath)):
+                texts = os.path.join(albumpath, album+".txt")
+                if os.path.isfile(texts):
+                    #todo: use user defined encoding
+                    parser.readfp(codecs.open(texts, "r", "utf8"))              
+                        
+                for i in os.listdir(albumpath):
+                    if os.path.isfile(os.path.join(albumpath, i)):
+                        alt   = " "
+                        title = " "
+                        iName, iExt = os.path.splitext(i)
+                        if iExt.lower() in IMAGE_EXTENSIONS: 
+                            if parser.has_option(iName, "alt"):
+                                alt = parser.get(iName, "alt")
+                            if parser.has_option(iName, "title"):
+                                title = parser.get(iName, "title")
+                            
+                            galleryimages.append(galleryimage(i, alt, title))
 
             article.album = album
-            article.galleryimages = sorted(galleryimages)
+            article.galleryimages = sorted(galleryimages, key=lambda galleryimage: galleryimage.src)
 
 
 def generate_gallery_page(generator):
